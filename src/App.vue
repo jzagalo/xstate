e4d31e613efc6020a93f6dd25306862c32580bec
Paul Ezaga <paulezaga@gmail.com>
2020-07-25 15:19:03 +0200

    Introduction to X-state

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="send('TOGGLE')">
+        {{ current.matches("inactive") ? "Off" : "On" }}
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Machine, interpret } from "xstate";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+
+@Component
+export default class App extends Vue {
+
+  private toggleMachine = Machine({
+    id: 'toggle',
+    context: {},
+    initial: 'inactive',
+    states: {
+      inactive: {
+        on: {
+          TOGGLE: 'active'
+        },
+      },
+      active: {
+        on: {
+          TOGGLE: 'inactive'
+        }
+      },   
+    },
+  });
+
+  private toggleService = interpret(this.toggleMachine);
+  private current = this.toggleMachine.initialState;
+  private context = this.toggleMachine.context;
+
+  private created(){
+    this.toggleService.onTransition(state => {
+      this.current  = state;
+      this.context = state.context;
+    }). 
+    start();   
+  }
+
+  private send(event: any){    
+    this.toggleService.send(event);
+  }
+}
+
 </script>
 
 <style lang="scss">
