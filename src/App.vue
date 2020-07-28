9fcb9ac670b2940fac2656ad7abfcfc534d1f108
Paul Ezaga <paulezaga@gmail.com>
2020-07-28 17:41:29 +0200

    Ajax animation Xstate Implementation

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,20 @@
 <template>
-  <div id="app">   
-    <div>
-    <b-tabs content-class="mt-3" v-model="tabIndex" justified>        
-        <b-tab title="Selecting Rocket" active>       
-          <div v-if="current.matches('selectRocket')">
-            <b-form v-on:submit="onNext"> 
-              <b-form-group id="input-group-2" label="Rocket:" label-for="input-2">
-                <b-form-input
-                  id="input-2"
-                  v-model="context.rocket"
-                  required
-                  placeholder="Enter Rocket" ></b-form-input>
-              </b-form-group>      
-              <b-button type="button" variant="primary" @click="onNext" >Next</b-button>
-              <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
-            </b-form>    
-          </div>      
-        </b-tab> 
-      
-      <b-tab title="Adding Payload">                 
-        <b-form v-on:submit="onNext" v-if="current.matches('addingPayload')">          
-          <b-form-group id="input-group-2" label="Payload:" label-for="input-2">
-            <b-form-input
-              id="input-2"
-              v-model="context.payload"
-              required
-              placeholder="Enter Rocket" ></b-form-input>
-          </b-form-group>      
-          <b-button type="button" variant="primary" @click="onNext">Next</b-button>
-          <b-button type="button" variant="danger" @click="onPrev">Prev</b-button>
-        </b-form> 
-      </b-tab>
-      <b-tab title="Launching">
-        <div v-if="current.matches('launching')">
-           We are lauching <strong>{{ current.context.payload}}</strong> on ("*")
-           <strong>{{ current.context.rocket }}</strong>(**) in 
-           <strong> {{ current.context.timeToLaunch }}</strong> seconds        
-        </div>
-      </b-tab>  
-      <b-tab title="Payload Deployed">
-        <div v-if="current.matches('payloadDeployed')">
-          Deployed Playload
-        </div>
-      </b-tab>       
-    </b-tabs>
-</div>
+  <div id="app"> 
+     <div v-if="current.matches('closed')" >
+       <p> Closed <button @click="() => send('OPEN')">OPEN </button></p>
+     </div>
+     <div v-if="current.matches('opening')" >
+       <p> Opening </p>
+     </div>
+     <div v-if="current.matches('open')" >
+      <p> Opened <button @click="() => send('CLOSE')">CLOSE</button> </p>
+     </div>
+      <div v-if="current.matches('closing')" >
+       <p> closing </p>
+     </div>
+     <div v-if="current.matches('error')" >
+       <p> Error <button @click="() => send('RESET')">RESET</button> </p>
+     </div>
   </div>
 </template>
 
@@ -54,100 +24,63 @@ import { Machine, interpret, assign } from "xstate";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-
+ 
 @Component
 export default class App extends Vue {
+  private openCartAnimation = () => new Promise((resolve, reject) => { setTimeout(() => resolve(true), 3000)});
+  private closeCartAnimation = () => new Promise((resolve, reject) => { setTimeout(() => resolve(false), 3000)});  
 
-  private toggleMachine = Machine({
-    id: 'toggle',
-    context: {
-      payload: '',
-      rocket: '',
-      timeToLaunch: 3,
-    },
-    initial: 'selectRocket',
+  private cartMachine = Machine({
+    id: 'cartComponent',
+    initial: 'closed',
     states: {
-      selectRocket: {
-        on: {         
-          NEXT: {
-            target : 'addingPayload',
-            cond: 'hasPayload',           
-          },
-        },
-      },
-      addingPayload: {
+      closed: {
         on: {
-          PREV: 'selectRocket',
-          NEXT: 'launching',
-        }
-      },  
-      launching: {
+          OPEN: 'opening',
+        },
+      },
+      opening: {
         invoke: {
-          src: () => (send) => {
-            const tim = window.setInterval(() =>{
-              send('TICK');
-            }, 1000);
-            
-            return () => { 
-              window.clearInterval(tim);              
-            };
-          },
-        },
-        on: {   
-          TICK:[
-            { 
-              target: "payloadDeployed",
-              cond: 'isDeployable',            
-            },
-            {
-              actions: ['sendTick']
-            },
-          ],          
-        },
-      }, 
-      payloadDeployed: {},   
-    },   
-    },       
-    {
-      guards: {
-        hasPayload: (context) => context.rocket.length > 2, 
-        isDeployable: (context) => context.timeToLaunch <= 0,   
+          src: this.openCartAnimation,
+          onDone: 'open',
+          onError: 'error',
+        },      
       },
-      actions: {
-        sendTick: assign({
-          timeToLaunch: ({ timeToLaunch }) => timeToLaunch - 1,
-        }),
+      open: {
+        on: {
+           CLOSE: 'closing',
+        },         
       },
-    });
+      closing: {
+        invoke: {
+          src: this.closeCartAnimation,
+          onDone: 'closed',
+          onError: 'error',          
+        },     
+      },
+      error: {
+        on: {
+          RESET: 'closed'
+        },
+      },
+    },           
+  });
 
-  private toggleService = interpret(this.toggleMachine);
-  private current = this.toggleMachine.initialState;
-  private context = this.toggleMachine.context;
+  private cartService = interpret(this.cartMachine);
+  private current = this.cartMachine.initialState;
+  private context = this.cartMachine.context;
   private tabIndex = 1;
 
   private created(){ 
-    this.toggleService.onTransition(state => {
+    this.cartService.onTransition(state => {
       this.current = state;
       this.context = state.context;
     }).start();   
   }
 
   private send(event: any){    
-    this.toggleService.send(event);
+    this.cartService.send(event);
   }
-
-  private onNext(event: any) {      
-    event.preventDefault();
-    this.send("NEXT");    
-    this.tabIndex++;
-    console.log(this.current);  
-  }  
-
-  private onPrev(event: any) {   
-    event.preventDefault();   
-    this.send("PREV");    
-    this.tabIndex--;
-  }   
 
 }
 
