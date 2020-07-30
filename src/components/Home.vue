907cbfe2ea3e890befe04f6c126c314a18c866fd
Paul Ezaga <paulezaga@gmail.com>
2020-07-30 14:16:54 +0100

    Fetching Stories from Backend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    Home   
+      <h1> HOME </h1>
+      <h2 v-if="current.matches('list.loading')">
+        Loading ...
+      </h2>
+       <div style="{color: 'red'}" v-if="current.matches('list.fail')">
+        Error Loading Stories {{context.error.toString() }}
+      </div>
+       <div v-if="current.matches('list.fail')">
+         {{context.stories }}
+      </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import cartMachine from '@/state';
+import { interpret } from 'xstate';
 
 @Component
 export default class Home extends Vue {
- 
+
+  private loadService = interpret(cartMachine);
+  private current = cartMachine.initialState;
+  private context = cartMachine.context;
+
+
+
+  private created(){
+    this.loadService.onTransition(state => {
+       this.current = state;
+       this.context = state.context;
+    })
+    .start();
+    this.loadService.send('LOAD_STORIES');  }
+
 }
 </script>
 
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,5 +1,17 @@
-import { Machine, interpret, assign } from "xstate";
+import { Machine, assign } from "xstate";
 import { auth } from '@/state/auth.js';
+
+const storiesUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+const getStoryDataUrl = (id: number) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
+
+
+const fetchStories = async () => {
+    const storyIds = await fetch(storiesUrl).then(r => r.json());
+    const topTenStories = await storyIds.slice(0,10).map((id: number) => getStoryDataUrl(id))
+    .map((url: string) => fetch(url).then(r => r.json()));
+    console.log({topTenStories});
+    return topTenStories;
+}
 
 
 
@@ -8,18 +20,47 @@ const cartMachine = Machine({
     initial: 'init',
     context: {
         user: undefined,
-        error: undefined,        
+        error: undefined,
+        stories: [],
     },
     states: {
         init: {},
         auth,
-    },    
-    on:{
+        list:{
+            initial: 'loading',
+            states: {
+                loading: {
+                    invoke: {
+                        id: 'fetchStories',
+                        src: fetchStories,
+                        onDone: {
+                            target: 'success',
+                            actions: assign({
+                                stories: (context, event) => event.data,
+                            }),
+                        },
+                        onError: {
+                            target: 'fail',
+                            actions: assign({
+                                error: (context, event) => event.data,
+                            }),
+                        },
+                    },
+                },
+                success: {},
+                fail: {},
+            },
+        },
+    },
+
+    on: {
         LOGIN: {
             target: 'auth.started'
-        }
+        },
+        LOAD_STORIES: {
+            target: 'list.loading',
+        },
     },
-           
 });
 
 
